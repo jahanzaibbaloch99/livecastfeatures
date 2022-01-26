7612d73b069c911e2fe3632a04031a69ec02f102
Jahanzaib Ali <jhanzeb.ali99@gmail.com>
2022-01-26 18:34:40 +0500

    added ffmpeg package

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FFmpegKit ,FFprobeKit} from 'ffmpeg-kit-react-native';
+import { useEffect } from 'react';
 
 export default function App() {
+  const myFile = require("./assets/sample.mp4")
+  useEffect(() => {
+    FFprobeKit.getMediaInformation(myFile).then(info => {
+      console.log('Result: ' + JSON.stringify(info));
+    }).catch((e) => {
+      console.log(e , "e")
+    })
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
